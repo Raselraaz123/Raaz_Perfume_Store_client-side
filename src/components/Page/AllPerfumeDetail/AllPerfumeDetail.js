27ef6915326a56f209eda738406a2b79e7afe564
Raselraaz123 <raselraaz91@gmail.com>
2022-05-08 15:32:19 +0600

    working update and deliverad but same problem have

--- src/components/Page/AllPerfumeDetail/AllPerfumeDetail.js
+++ src/components/Page/AllPerfumeDetail/AllPerfumeDetail.js
@@ -8,19 +8,26 @@ const AllPerfumeDetail = () => {
   const [perfume] = usePerfumeDetail(perfumeId)
   console.log(perfume)
   return (
-    <div className="prefume-Detail-container">
-      <div>
-        <p>
-          <strong>Id : </strong> {perfume._id}
-        </p>
-        <h1>Name : {perfume.name}</h1>
-        <h4>price:{perfume.price}</h4>
-        <img height={250} src={perfume.picture} alt="" />
-      </div>
-      <div>
-        <h4>Quantity : {perfume.Quantity}</h4>
-        <input type="number" name="" id="" /><br />
-        <button className=' btn btn-primary mt-2'>Deliverad</button>
+    <div>
+      <div className="prefume-Detail-container">
+        <div>
+          <p>
+            <strong>Id : </strong> {perfume._id}
+          </p>
+          <h1>Name : {perfume.name}</h1>
+          <h4>price:{perfume.price}</h4>
+          <img height={250} src={perfume.picture} alt="" />
+          <h4>Quantity : {perfume.Quantity}</h4>
+          <input type="number" name="" id="" />
+          <br />
+          <button className=" btn btn-primary mt-2">Deliverad</button>
+        </div>
+        <div className='bg-color '>
+          <h2 className="text-center">Update quantity From</h2>
+          <label htmlFor="">Quantity</label><br />
+          <input type="number" name="number" id="" placeholder="quantity" /><br />
+          <button className='btn btn-primary mt-2'>Update</button>
+        </div>
       </div>
     </div>
   );
